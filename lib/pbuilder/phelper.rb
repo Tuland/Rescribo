8025209adc48baf5ab8c7533db9206a355940bec
Tullio <tuland@gmail.com>
2011-02-10 19:55:33 +0100

    Comments: Phelper

--- lib/pbuilder/phelper.rb
+++ lib/pbuilder/phelper.rb
@@ -8,6 +8,12 @@ module Pbuilder
   
   class Phelper
     
+    # Set constant associated with root concept for +klass+
+    # (testing mode)
+    # 
+    # ==== Attributes  
+    #  
+    # * +klass+ - A Class to extend
     def self.set_rootc_const_for_test(klass)
       klass.const_set("ABSTRACT_CONCEPT_STR", "http://www.siti.disco.unimib.it/prova#A1.AbstractConcept")
       klass.const_set("ABSTRACT_CONCEPT_RES", RDFS::Resource.new(eval "#{klass}::ABSTRACT_CONCEPT_STR"))
@@ -15,6 +21,14 @@ module Pbuilder
       klass.const_set("CORE_CONCEPT_RES", RDFS::Resource.new(eval "#{klass}::CORE_CONCEPT_STR"))
     end
     
+    # Set constant associated with properties for +klass+
+    # (testing mode)
+    #  
+    # ==== Attributes  
+    #  
+    # * +klass+ - A Class to extend
+    # * +first_letter+ - The first letter that itentified the first constant
+    # * +last_letter+ - The last letter that itentified the last constant
     def self.set_concepts_const_for_test(klass, first_letter, last_letter)
       j = 2
       for i in first_letter..last_letter do
@@ -24,6 +38,14 @@ module Pbuilder
       end
     end
     
+    # Set constant associated with concept for +klass+
+    # (testing mode)
+    # #  
+    # ==== Attributes  
+    #  
+    # * +klass+ - A Class to extend
+    # * +first_letter+ - The first letter that itentified the first constant
+    # * +last_letter+ - The last letter that itentified the last constant
     def self.set_properties_const_for_test(klass, first_letter, last_letter)
       j = 1
       for i in first_letter..last_letter do
